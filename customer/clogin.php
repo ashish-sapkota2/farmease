2f0269ea403598fdd8aa0360f30a514eb149ab55
Sanisha <sanisamhrzn@gmail.com>
2023-05-31 12:47:41 +0545

    farmer and customer login page changed

--- customer/clogin.php
+++ customer/clogin.php
@@ -9,28 +9,26 @@ include('cloginScript.php'); // Includes Login Script.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="icon" type="image/png" href="../assets/img/logo.png" />
-    <title>Agriculture Portal</title>
+  <!-- <link rel="icon" type="image/png" href="../assets/img/logo.png" /> -->
+    <title>FarmEase</title>
 
   <!--     Fonts and icons     -->
   
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" 
-	integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-	
-	<link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css "/>
-	
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-  
-  <!-- Nucleo Icons -->
-  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" 
+integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
+<link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css "/>
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+<!--Nucleo Icons-->
+<link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+<link href="assets/css/nucleo-svg.css" rel="stylesheet" />
  
-  <link rel="stylesheet" href="../assets/css/creativetim.min.css" type="text/css">
+<link rel="stylesheet" href="../assets/css/theme.css" type="text/css"> 
 
 </head>
 
@@ -38,76 +36,18 @@ include('cloginScript.php'); // Includes Login Script.
     <!-- Navbar -->
     <nav
       id="navbar-main"
-      class="
-        navbar navbar-main navbar-expand-lg
-        bg-default
-        navbar-light
-        position-sticky
-        top-0
-        shadow
-        py-0
-      "
-    >
-      <div class="container">
-        <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
-          <li class="nav-item dropdown">
-            <a href="../index.php" class="navbar-brand mr-lg-5 text-white">
-                               <img src="../assets/img/nav.png" />
-            </a>
-          </li>
-        </ul>
-
-        <button
-          class="navbar-toggler bg-white"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbar_global"
-          aria-controls="navbar_global"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon text-white"></span>
-        </button>
-        <div class="navbar-collapse collapse bg-default" id="navbar_global">
-          <div class="navbar-collapse-header">
-            <div class="row">
-              <div class="col-10 collapse-brand">
-                <a href="../index.html">
-                  <img src="../assets/img/nav.png" />
-                </a>
-              </div>
-              <div class="col-2 collapse-close bg-danger">
-                <button
-                  type="button"
-                  class="navbar-toggler"
-                  data-toggle="collapse"
-                  data-target="#navbar_global"
-                  aria-controls="navbar_global"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span></span>
-                  <span></span>
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <ul class="navbar-nav align-items-lg-center ml-auto">
-		  
-		   <li class="nav-item">
-              <a href="../contact.php" class="nav-link">
-                <span class="text-white nav-link-inner--text"
-                  ><i class="text-white fas fa-address-card"></i> Contact</span
-                >
-              </a>
-            </li>
-			
-						  <li class="nav-item">
-			   <div class="dropdown show ">
-		  <a class="nav-link dropdown-toggle text-white " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		                  <span class="text-white nav-link-inner--text"
-                  ><i class="text-white fas fa-user-plus"></i> Sign Up</span
+      class="navbar navbar-expand-lg navbar-light fixed-top py-3 bg-light opacity-85" data-navbar-on-scroll="data-navbar-on-scroll">
+      <div class="container"><a class="navbar-brand" href="index.php"><img class="d-inline-block align-top img-fluid" src="../assets/img/fm-logo.png" alt="" width="100" /><span class="text-theme font-monospace fs-4 ps-2"></span></a>
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+         <div class="navbar-collapse collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+		  <li class="nav-item px-2"><a class="nav-link fw-medium active" aria-current="page" href="index.php">Home</a></li>
+		  <li class="nav-item px-2"><a class="nav-link fw-medium" aria-current="page" href="contact.php">Contact</a></li>
+          <li class="nav-item px-2"><a class="nav-link fw-medium" href="index.php">Opportunities</a></li>
+          <li class="nav-item px-2"><div class="dropdown show">
+		  <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		                  <span class="nav-link-inner--text"
+                  ><i class="fas fa-user-plus"></i> Sign Up</span
                 >
 		  </a>
 
@@ -119,32 +59,32 @@ include('cloginScript.php'); // Includes Login Script.
 			</li>
 			
 		  
-		  	  <li class="nav-item">
+				  <li class="nav-item px-2">
 			   <div class="dropdown show ">
-		  <a class="nav-link dropdown-toggle text-success " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		                  <span class="text-success nav-link-inner--text"
-                  ><i class="text-success fas fa-sign-in-alt"></i> Login</span
+		  <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		                  <span class="nav-link-inner--text"
+                  ><i class="fas fa-sign-in-alt"></i> Login</span
                 >
 		  </a>
 
-		   <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-			<a class="dropdown-item" href="../farmer/flogin.php">Farmer</a>
-			<a class="dropdown-item" href="clogin.php">Customer</a>
-			<a class="dropdown-item" href="../admin/alogin.php">Admin </a>
+		  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+			<a class="dropdown-item" href="farmer/flogin.php">Farmer</a>
+			<a class="dropdown-item" href="customer/clogin.php">Customer</a>
+			<a class="dropdown-item" href="admin/alogin.php">Admin </a>
 		  </div>
 		</div>
 			</li>
-           
-
+			
           
           </ul>
+          
         </div>
       </div>
     </nav>
     <!-- End Navbar -->
  	
   <section class="section section-shaped section-lg">
-    <div class="shape shape-style-1 shape-primary">
+    <!-- <div class="shape shape-style-1 shape-primary">
       <span></span>
       <span></span>
       <span></span>
@@ -155,22 +95,22 @@ include('cloginScript.php'); // Includes Login Script.
       <span></span>
       <span></span>
       <span></span>
-    </div>
+    </div> -->
 <!-- ======================================================================================================================================== -->
 
 <div class="container">
 
- <div class="row">
+ <!-- <div class="row">
           <div class="col-md-8 mx-auto text-center">
             <span class="badge badge-info badge-pill mb-3">Login</span>
           </div>
-        </div> 
+        </div>  -->
       
 <div class="row">
 <div class="col-sm-12 mb-3">  
 			  
   <div class="nav nav-tabs nav-fill bg-gradient-default" id="nav-tab" role="tablist">
-    <a class="nav-item nav-link active font-weight-bold text-warning" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Customer Login</a>
+    <a class="nav-item nav-link active font-weight-bold text-color" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Customer Login</a>
 
   </div>
                   
@@ -188,7 +128,7 @@ include('cloginScript.php'); // Includes Login Script.
                         
 					<div class="form-group row">
                     <label for="email" class="col-md-3 col-form-label"
-                      ><h6 class="text-white font-weight-bold">Email Id</h6>
+                      ><h6 class="text-dark font-weight-bold">Email Id</h6>
                     </label>
                     <div class="col-md-9">
                       <input
@@ -202,7 +142,7 @@ include('cloginScript.php'); // Includes Login Script.
                   </div>
 				  
 				  			  		 <div class="form-group row">
-                <label for="pass" class="col-md-3 col-form-label text-white" > Password </label>
+                <label for="pass" class="col-md-3 col-form-label text-dark font-weight-bold" > Password </label>
 			   <div class="col-9">
             <div class="input-group mb-3">
               <div class="input-group-prepend">
@@ -225,7 +165,7 @@ include('cloginScript.php'); // Includes Login Script.
                     <div class="offset-md-3 col-md-2">
                       <button
                         type="submit"
-                        class="btn btn-info text-dark"
+                        class="btn btn-block text-white btn-success"
 						name="customerlogin" id="submit" >
                         Login
                       </button>
@@ -264,6 +204,9 @@ if (isset($_GET['error'])) {
 </section>
 
 	    <?php require("footer.php");?>
+ <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Chivo:wght@300;400;700;900&amp;display=swap" rel="stylesheet">
 
 </body>
 
